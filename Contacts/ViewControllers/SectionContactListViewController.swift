424fdc178e0a8fdbc114a49e0a803415c83671b1
Gurina Elena <belka2239@gmail.com>
2022-03-22 21:00:02 +0300

    SectionContactListViewController

---
+++ Contacts/ViewControllers/SectionContactListViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SectionContactListViewController.swift
+//  Contacts
+//
+//  Created by VG on 17.03.2022.
+//
+
+import UIKit
+
+class SectionContactListViewController: UITableViewController {
+    
+    var persons: [Person] = []
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        persons.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        persons[section].fullName
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "contactLine", for: indexPath)
+        
+        let person = persons[indexPath.section]
+        var content = cell.defaultContentConfiguration()
+        
+        switch indexPath.row {
+        case 0:
+            content.text = person.phoneNumber
+        default:
+            content.text = person.email
+        }
+        
+        cell.contentConfiguration = content
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
