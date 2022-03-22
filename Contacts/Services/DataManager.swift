30db8f69280c7bf06d6f39318a55eb4a27a77bca
Gurina Elena <belka2239@gmail.com>
2022-03-22 20:58:24 +0300

    Creating  DataManager

---
+++ Contacts/Services/DataManager.swift
@@ -0,0 +1,15 @@
+//
+//  DataManager.swift
+//  Contacts
+//
+//  Created by VG on 20.03.2022.
+//
+
+import Foundation
+
+class DataManager {
+    let names = ["John", "Aaron", "Tim", "Ted", "Steven"]
+    let surnames = ["Smith", "Dow", "Isaacson", "Pennyworth", "Jankins"]
+    let phones = ["+79251112233", "+79252223344", "+79253334455", "+79254445566", "+79255556677"]
+    let emails = ["qqq@gmail.com", "aaa@gmail.com", "eee@gmail.com", "ttt@gmail.com", "ggg@gmail.com"]
+}
